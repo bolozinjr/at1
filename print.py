bc446e62999fe3202e1e974db51bdcbdf3d8d979
Marcos Vinicios <168040265+bolozinjr@users.noreply.github.com>
2024-11-25 19:49:17 -0300

    Soma funcionalidade
    Adicionando uma soma ao código

--- print.py
+++ print.py
@@ -1,1 +1,8 @@
 print("nova funcionalidade, porém, isso é uma correção")
+
+def soma(a, b):
+    return a + b
+
+# Testando a função
+resultado = soma(5, 3)
+print("O resultado da soma é:", resultado)
